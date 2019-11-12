f0c9f4740a05eb1b6fe53e2a8c642f600b69e027
L-xuanda <git config --global user.email>
2019-11-12 01:13:48 -0800

    bitcount

--- first.c
+++ first.c
@@ -1,32 +1,25 @@
 #include <stdio.h>
-#include<math.h>
-int bitcount(unsigned x);
+ int bitcount(unsigned x);
 int main()
 
 {
+    int n = 0;
+    int i = 0;
     char digit[6];
-    int ndigit[6];
 
-    int i, t, a, g, h, j, k, l;
-    int input;
-    i = 0, a = 0;
-    while ((digit[i] = getchar()) != EOF && digit[i] != '\n')
+    while (i < 7)
     {
-
-        ndigit[i] = digit[i] - '0';
-        ++i;
-        a = a + 1;
+     digit[i]=getchar();
+        if (digit[i] == '\n')
+        {
+        break;
         }
-        g = ndigit[0];
-        h = ndigit[1];
-        j  = ndigit[2];
-        k = ndigit[3];
-        l = ndigit[4];
-
-        t = g * pow(10, a) + h * pow(10, a - 1) + j * pow(10, a - 2) + k * pow(10, a - 3) + l * pow(10, a - 4);
-        printf("%d\n", bitcount(t));
+            n = n * 10 +( digit[i] - '0');
+            i++;
+    }
+    printf("%d",bitcount(n));
 }
-int bitcount(unsigned x)
+    int bitcount(unsigned x)
 {
     int b;
     for (b = 0; x != 0; x >>= 1)
